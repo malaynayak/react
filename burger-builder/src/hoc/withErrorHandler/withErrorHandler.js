62a4e7f3fef075036f879e7588f35b5c9f44ebb1
Nayak <806279@cognizant.com>
2019-09-23 17:58:03 +0530

    Accesing Server - Remove Interceptors

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,14 +11,19 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
 
         // This is deprecated, use constructor. 
         componentWillMount = () => {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
             });
 
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             });
+        }
+
+        componentWillMount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler = () => {
