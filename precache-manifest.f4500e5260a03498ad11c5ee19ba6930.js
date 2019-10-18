0286e076f99667f5aa7740c06afc6830be9665d7
Malay Nayak <malay.kanta.nayak@gmail.com>
2019-10-18 19:47:40 +0530

    Updates

---
+++ precache-manifest.f4500e5260a03498ad11c5ee19ba6930.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90642e163c6ac28f14f0cd45c9af95e",
+    "url": "/react/burger-builder/index.html"
+  },
+  {
+    "revision": "2c6fbb622f53c0831ae2",
+    "url": "/react/burger-builder/static/css/3.ec77eabb.chunk.css"
+  },
+  {
+    "revision": "f9138399a335f510402f",
+    "url": "/react/burger-builder/static/css/4.f5fe29d5.chunk.css"
+  },
+  {
+    "revision": "7b540e712bc5f8a6ffa5",
+    "url": "/react/burger-builder/static/css/5.9fc637ef.chunk.css"
+  },
+  {
+    "revision": "e69ced60f50e1edfd05f",
+    "url": "/react/burger-builder/static/css/main.1ee68ee5.chunk.css"
+  },
+  {
+    "revision": "86c38efe8f480f58b3c7",
+    "url": "/react/burger-builder/static/js/2.13b0cf0e.chunk.js"
+  },
+  {
+    "revision": "2c6fbb622f53c0831ae2",
+    "url": "/react/burger-builder/static/js/3.94bb4bca.chunk.js"
+  },
+  {
+    "revision": "f9138399a335f510402f",
+    "url": "/react/burger-builder/static/js/4.9eaf5b39.chunk.js"
+  },
+  {
+    "revision": "7b540e712bc5f8a6ffa5",
+    "url": "/react/burger-builder/static/js/5.e52910ad.chunk.js"
+  },
+  {
+    "revision": "e69ced60f50e1edfd05f",
+    "url": "/react/burger-builder/static/js/main.bdc4f223.chunk.js"
+  },
+  {
+    "revision": "6a1e5d1015f26afabba2",
+    "url": "/react/burger-builder/static/js/runtime~main.fb8fe9f2.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
